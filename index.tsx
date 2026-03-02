82bda300483e7ab0167a93cb91fa7bf48aee75b9
mattonenine-cpu <matt.onenine@gmail.com>
2026-03-02 14:24:24 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import './index.css'
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
